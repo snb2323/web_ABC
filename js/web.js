66f375d3f7a011cd9120739710549c208949cd89
snb2323 <dheltus1r@naver.com>
2023-11-03 18:02:34 +0900

    상하스와이퍼완료

--- js/web.js
+++ js/web.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
     setInterval(function () {
-        $(".swiper-wrapper").animate({ "marginTop": 1200 }, 400, function () {
+        $(".swiper-wrapper").animate({ "marginTop": -300 }, 400, function () {
             $(".swiper-wrapper div:first-child").appendTo($(".swiper-wrapper"))
             $(".swiper-wrapper").css("marginTop", 0)
         })
